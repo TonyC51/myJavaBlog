5d0927586d659a8ef07f347873deda8f0e25918a
Tony CHEVRIS <tony.chevris@gmail.com>
2018-12-08 18:38:38 +0100

    added getters and setters

--- src/com/projet/Post.java
+++ src/com/projet/Post.java
@@ -13,4 +13,28 @@ public class Post {
 		
 	}
 	
+	public int getId () {
+		return this.id;
+	}
+	
+	public void setId (int id) {
+		this.id = id;
+	}
+	
+	public String getTitle () {
+		return this.title;
+	}
+	
+	public void setTitle (String title) {
+		this.title = title;
+	}
+	
+	public String getContent () {
+		return this.content;
+	}
+	
+	public void setContent (String content) {
+		this.content = content;
+	}
+	
 }
